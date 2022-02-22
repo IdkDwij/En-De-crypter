1697184535f42e32f13ed54115bc5465a747697f
Dwij <94942567+IdkDwij@users.noreply.github.com>
2022-02-22 14:36:51 +0000

    quality of life changes

--- crypter.py
+++ crypter.py
@@ -1,54 +1,53 @@
-try:
-  import base64
-except:
-  print("Base 64 Package not installed")
+import os
+import base64
 
-contents = ""
+
 content = ""
 
 
 crypt = input("Decrypt(d) or Encrypt(e): ")
 level = input("Binary encrypter/decrypter[b] or base64 encrypter/decrypter[base]")
-if level == "b":
-  if crypt == "d":
-    getpath = input("enter file path: ")
-    with open(getpath) as f:
-      contents = f.read()
-    f.close()
-    decrypted = "".join([chr(int(binary, 2)) for binary in contents.split(" ")])
-    with open('decryptedfile.txt', 'w') as e:
-      e.write(decrypted)
-    e.close()
+getpath = input("enter file path: ")
 
-  elif crypt == "e":
-    getfilepath = input("enter file path: ")
-    with open(getfilepath) as f:
-      content = f.read()
+if os.path.isfile(getpath) == True:
+  if level == "b":
+    if crypt == "d":
+      with open(getpath) as f:
+        content = f.read()
       f.close()
-    binaryencrypt = ' '.join(format(ord(x), 'b') for x in content) 
-    with open('cryptedfile.binary', 'w') as r:
-      r.write(binaryencrypt)
-    r.close()
+      decrypted = "".join([chr(int(binary, 2)) for binary in content.split(" ")])
+      with open('decryptedfile.txt', 'w') as e:
+        e.write(decrypted)
+      e.close()
 
-if level == "base":
-  if crypt == "d":
-    getpath = input("enter file path: ")
-    with open(getpath) as f:
-      contents = f.read()
-    f.close()
-    decodedBytes = base64.b64decode(contents)
-    decodedStr = str(decodedBytes, "utf-8")
-    with open('decryptedfile.txt', 'w') as e:
-      e.write(decodedStr)
-    e.close()
+    elif crypt == "e":
+      with open(getpath) as f:
+        content = f.read()
+        f.close()
+      binaryencrypt = ' '.join(format(ord(x), 'b') for x in content) 
+      with open('cryptedfile.encrypt', 'w') as r:
+        r.write(binaryencrypt)
+      r.close()
 
-  elif crypt == "e":
-    getfilepath = input("enter file path: ")
-    with open(getfilepath) as f:
-      content = f.read()
+  if level == "base":
+    if crypt == "d":
+      with open(getpath) as f:
+        content = f.read()
       f.close()
-    urlSafeEncodedBytes = base64.urlsafe_b64encode(content.encode("utf-8"))
-    urlSafeEncodedStr = str(urlSafeEncodedBytes, "utf-8") 
-    with open('cryptedfile.base64', 'w') as r:
-      r.write(urlSafeEncodedStr)
-    r.close()
+      decodedBytes = base64.b64decode(content)
+      decodedStr = str(decodedBytes, "utf-8")
+      with open('decryptedfile.txt', 'w') as e:
+        e.write(decodedStr)
+      e.close()
+
+    elif crypt == "e":
+      with open(getpath) as f:
+        content = f.read()
+        f.close()
+      urlSafeEncodedBytes = base64.urlsafe_b64encode(content.encode("utf-8"))
+      urlSafeEncodedStr = str(urlSafeEncodedBytes, "utf-8") 
+      with open('cryptedfile.encrypt', 'w') as r:
+        r.write(urlSafeEncodedStr)
+      r.close()
+else:
+  print("invalid file path")
